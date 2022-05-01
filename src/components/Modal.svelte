4e37fc4702e1cb6003ee15b898400d5ef7d64425
jesse-rb <jesse.reynekebarnard@gmail.com>
2022-05-01 17:29:10 +1200

    Improve modal cinema mode with full screen overlay behind modal

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -7,27 +7,45 @@
 </script>
 
 <div id="modal-component" class="{open ? 'open' : ''} {cinema ? 'cinema' : ''}">
-    <Button label="x" onclick={toggle} />
-    <slot></slot>
+    <div id="modal-component-inner">
+        <Button label="x" onclick={toggle} />
+        <slot></slot>
+    </div>
 </div>
 
 <style>
     #modal-component {
+        display: none;
+    }
+    #modal-component-inner {
         display: none;
         position: relative;
         background-color: #2c2035;
         border-radius: 10px;
         padding: 10px;
         margin: 10px;
-        box-shadow: 0 0 5px #352979;
     }
-    #modal-component.cinema {
+    #modal-component.cinema.open {
+        display: block;
         position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 1000;
+        background-color: rgba(56, 54, 67, 0.7);
+    }
+    #modal-component.cinema.open #modal-component-inner {
         margin: 0 auto 0 auto;
-        top: 33%;
-
+        top: 25%;
+        width: 50%;
+        height: 50%;
+        z-index: 1050;
     }
     #modal-component.open {
+        display: contents;
+    }
+    #modal-component.open #modal-component-inner {
         display: block;
     }
 
