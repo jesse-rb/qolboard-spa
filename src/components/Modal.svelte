d0d8079af06fe966c40ede19f3cd8ca0ce229e9c
jesse-rb <jesse.reynekebarnard@gmail.com>
2022-05-21 14:26:01 +1200

    Refactor header, added svelte transitions for modals

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,31 +1,50 @@
 <script>
+    import { elasticOut } from 'svelte/easing';
+    import { slide } from 'svelte/transition';
     import Button from './Button.svelte';
     export let cinema = false;
     let open = false;
     export const toggle = () => {open = !open};
 
+    function transitionCloseButton(node, { delay }) {
+        return {
+            delay,
+            duration: 800,
+            easing: elasticOut,
+            css: (t, u) => `
+                transform: scale(${t});
+            `
+        }
+    }
 </script>
 
-<div id="modal-component" class="{open ? 'open' : ''} {cinema ? 'cinema' : ''}">
-    <div id="modal-component-inner">
-        <Button label="x" onclick={toggle} />
-        <slot></slot>
+
+{#if open}
+    <div class="modal-component" class:cinema={cinema}>
+        <div in:slide out:slide="{{ delay:400 }}" class="modal-component-inner">
+            <div in:transitionCloseButton="{{delay: 400}}" out:transitionCloseButton="{{delay: 0}}" class="close-button">
+                <Button label="x" onclick={toggle} />
+            </div>
+            <slot></slot>
+        </div>
     </div>
-</div>
+{/if}
 
 <style>
-    #modal-component {
-        display: none;
+    .modal-component {
+        display: contents;
     }
-    #modal-component-inner {
+    .modal-component-inner {
         display: none;
         position: relative;
         background-color: #2c2035;
         border-radius: 10px;
         padding: 10px;
-        margin: 10px;
+        margin: 30px;
+        /* box-shadow: #8d4848 -1px 1px 1px 0px; */
+        border: #383643 1px solid;
     }
-    #modal-component.cinema.open {
+    .modal-component.cinema {
         display: block;
         position: absolute;
         top: 0;
@@ -35,21 +54,18 @@
         z-index: 1000;
         background-color: rgba(56, 54, 67, 0.7);
     }
-    #modal-component.cinema.open #modal-component-inner {
+    .modal-component.cinema .modal-component-inner {
         margin: 0 auto 0 auto;
         top: 25%;
         width: 50%;
         height: 50%;
         z-index: 1050;
     }
-    #modal-component.open {
-        display: contents;
-    }
-    #modal-component.open #modal-component-inner {
+    .modal-component .modal-component-inner {
         display: block;
     }
 
-    #modal-component :global(#button-component) {
+    .modal-component .close-button {
         position: relative;
         float: right;
         right: -20px;
--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -1,28 +1,71 @@
 <script>
     import { Link } from 'svelte-navigator';
-    import Button from '../components/Button.svelte';
     import Modal from '../components/Modal.svelte';
+    import Button from '../components/Button.svelte';
 
+    let menuModal;
     let loginModal;
     let registerModal;
     let aboutModal;
 </script>
 
-<div id="header-layout">
-    <Modal bind:this={loginModal} cinema={true}>
-        <p>Login modal</p>
-    </Modal>
-    <Button label="Login" onclick={ () => loginModal.toggle() }/>
-    <Button label="Register" />
-    <Button label="About" />
-    <h1>welcome to qolboard 2.0</h1>
+<div class="header-layout">
+    <div class="banner">
+        <h1>Welcome to qolboard 2.0</h1>
+        <Button label="Menu" onclick={()=>menuModal.toggle()} />
+    </div>
+    <div class="menu">
+        
+    </div>
 </div>
+<Modal bind:this={menuModal}>
+    <Button label="Login" onclick={()=>loginModal.toggle()} />
+    <Button label="Register" onclick={()=>registerModal.toggle()} />
+    <Button label="About" onclick={()=>aboutModal.toggle()} />
+
+    <Modal bind:this={loginModal}>
+        <h2>Login</h2>
+    </Modal>
+
+    <Modal bind:this={registerModal}>
+        <h2>Register</h2>
+    </Modal>
+
+    <Modal bind:this={aboutModal}>
+        <h2>Privacy note</h2>
+        <p>Please note that qolboard uses:</p>
+        <ul>
+            <li>session storage to carry your drawings over between page refreshes</li>
+            <li>server memory to sync your drawing with anyone who joins the 'room' which you are currently in.</li>
+        </ul>
+        <p>Therefore <em>please do not use qolboard for any personal or private details</em>.</p>
+    </Modal>
+</Modal>
 
 <style>
-    #header-layout {
+    .header-layout {
         display: flex;
         align-items: center;
-        justify-content: space-evenly;
         background-color: #754848;
+        align-items: stretch;
+    }
+    .banner {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 2;
+        padding: 20px;
+    }
+    .banner :global(.button-component) {
+        margin-left: 10px;
+    }
+    .menu {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
+    }
+    h1 {
+        margin: 0;
     }
 </style>
