ed694f99f1765ee2cd3ad8c92d27c021dbcb4b93
jesse-rb <jesse.reynekebarnard@gmail.com>
2022-08-13 18:44:42 +1200

    set base for github pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,15 @@
 <script>
-    // This is the app's page and can be used for routing
-    import { Router, Route } from 'svelte-navigator';
     import Header from './lib/layout/Header.svelte';
     import Home from './lib/pages/Home.svelte';
 </script>
 
 <main>
-    <Router>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <Header />
-        <div id="page">
-                <Route>
-                        <Home />
-                </Route>
-                <Route path="/board">
-                        <!-- <Board /> -->
-                </Route>
-        </div>
-        <!-- <Footer /> -->
-    </Router>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <Header />
+    <div id="page">
+        <Home />
+    </div>
+    <!-- <Footer /> -->
 </main>
 
 <style>
