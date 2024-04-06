696c828f1cf640bb8110eb030133a8275a1e4ce1
jesse-rb <jesse.reynekebarnard@gmail.com>
2024-04-06 20:41:14 +1300

    Add simple qolboard favicon

--- src/lib/components/Canvas/Ruler.svelte
+++ src/lib/components/Canvas/Ruler.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import { colorIsDark, range, roundToInt } from "../../util";
-    import { getContext } from "svelte";
+    import { colorIsDark, range } from "../../util";
+    import { getContext, onMount } from "svelte";
     import type { CanvasStore } from "./types/canvas.js";
     import type { Writable } from "svelte/store";
 
     const canvasStore:Writable<CanvasStore> = getContext('canvasStore');
     let stepX:number = 100;
-    let toX:number = $canvasStore.width;
-    let fromX:number = 0;
+    // let toX:number = $canvasStore.width;
+    // let fromX:number = 0;
     let rangeX:Array<number> = [];
     let adderPanX:number = 0;
 
@@ -16,28 +16,35 @@
     let nextRangeZoomOut = $canvasStore.zoom;
     let adderZoomX = 0;
     
-    $: if ($canvasStore.width || adderPanX || adderZoomX) {
-        toX = ($canvasStore.width + adderPanX - adderZoomX);
-        fromX = (0 + adderPanX + adderZoomX);
-    }
+    $: width = $canvasStore.width;
+    
+    // $: if ($canvasStore.width || adderPanX || adderZoomX) {
+    //     toX = ($canvasStore.width + adderPanX - adderZoomX);
+    //     fromX = (0 + adderPanX + adderZoomX);
+    // }
 
-    let ZERO = 0;
-    $: if (toX, fromX, stepX) {
-        rangeX = range(toX, fromX, stepX);
-        console.log(rangeX);
+    // let ZERO = 0;
+    // $: if (toX, fromX, stepX) {
+    //     rangeX = range(toX, fromX, stepX);
+    //     console.log(rangeX);
 
-        if (!ZERO) {
-            const i = Math.floor(rangeX.length/2);
-            ZERO = rangeX[i];
-        }
-    }
+    //     if (!ZERO) {
+    //         const i = Math.floor(rangeX.length/2);
+    //         ZERO = rangeX[i];
+    //     }
+    // }
 
     // Update our ruler to/form range when panning the canvas
-    $: if (((-1) * $canvasStore.xPan+$canvasStore.zoomDx) > ((adderPanX-adderZoomX) + stepX)) {
-        adderPanX = (adderPanX + stepX);
+    $: if (((-1) * $canvasStore.xPan+$canvasStore.zoomDx+$canvasStore.width) > (rangeX[rangeX.length-1] + stepX)) {
+        // adderPanX = (adderPanX + stepX);
+        const iEnd = rangeX.length-1;
+        const end = rangeX[iEnd];
+        rangeX = [ ...rangeX.slice(1), end + stepX ];
     }
-    $: if (((-1) * ($canvasStore.xPan+$canvasStore.zoomDx)) < ((adderPanX+adderZoomX) - stepX)) {
-        adderPanX = (adderPanX - stepX);
+    $: if (((-1) * ($canvasStore.xPan+$canvasStore.zoomDx)) < (rangeX[0] - stepX)) {
+        // adderPanX = (adderPanX - stepX);
+        const start = rangeX[0];
+        rangeX = [ start - stepX, ...rangeX.slice(0, -1) ];
     }
 
     // IF zoom has increased by >= .5 of zoom
@@ -48,29 +55,36 @@
     //  AND stepX needs to be extended by 2*stepX
 
     $: if ($canvasStore.zoom >= nextRangeZoomIn) {
-        // zoomInRange();
+        zoomInRange();
     }
 
     $: if ($canvasStore.zoom < nextRangeZoomOut) {
         zoomOutRange();
-        stepX = stepX;
     }
 
-    // $: console.log(`fromX: ${fromX}`);
-    // $: console.log(`toX: ${toX}`);
-    // $: console.log(rangeX);
+    onMount(() => {
+        rangeX = range(width, 0, stepX);
+    });
 
-    // function zoomInRange() {
-    //     // Set the next range zoom stepping points
-    //     nextRangeZoomOut = nextRangeZoomIn;
-    //     nextRangeZoomIn = nextRangeZoomIn * 1.5;
+    function zoomInRange() {
+        // Set the next range zoom stepping points
+        nextRangeZoomOut = nextRangeZoomIn;
+        nextRangeZoomIn = nextRangeZoomIn * 1.5;
+
+        const newStepX = stepX * 0.5;
         
-    //     // Trim range
-    //     adderZoomX += Math.floor(rangeX.length/4)*stepX;
-        
-    //     // Update stepX
-    //     stepX *= 0.5;
-    // }
+        // Update stepX
+        stepX = newStepX;
+
+        // Update range start/end
+        const iEnd = rangeX.length - 1;
+        const end = rangeX[iEnd] - stepX*4;
+        const start = rangeX[0] + stepX*4;
+
+        // Update range
+        rangeX = range(end, start, stepX);
+        console.log(rangeX);
+    }
 
     function zoomOutRange() {
         // Set the next range zoom stepping points
@@ -79,21 +93,26 @@
 
         const newStepX = stepX * 2;
         
-        // Extend range
-        adderZoomX -= stepX*4;
-        
         // Update stepX
         stepX = newStepX;
+
+        // Update range start/end
+        const iEnd = rangeX.length - 1;
+        const end = rangeX[iEnd] + stepX*4;
+        const start = rangeX[0] - stepX*4;
+
+        // Update range
+        rangeX = range(end, start, stepX);
+        console.log(rangeX);
     }
 
 </script>
 
 <div class="x-ruler pointer-events-none {colorIsDark($canvasStore.backgroundColor) ? 'text-white' : 'text-black'}">
     {#each rangeX as i}
-        {@const display = i - ZERO}
         {@const pos = ( (i + $canvasStore.xPan + $canvasStore.zoomDx) * $canvasStore.zoom ) }
 
-        <span class="absolute font-mono" style="left: {pos}px;" >{display.toFixed(0)}</span>
+        <span class="absolute font-mono" style="left: {pos}px;" >{i.toFixed(0)}</span>
     {/each}
 </div>
 
