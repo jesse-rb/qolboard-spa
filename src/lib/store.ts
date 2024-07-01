fc12b3a34edc7c5c64d91893eca01f5a1b4775db
jesse-rb <jesse.reynekebarnard@gmail.com>
2024-07-01 21:26:46 +1200

    WIP: routing

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,19 +1,21 @@
-import { get, writable, type Writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
 
 export type AppStore = {
     isAuthenticated: boolean
     email: string
     headerHeight: number
     controlPanelHeight: number
+    page: string
 }
 
 export const appStore:Writable<AppStore> = writable({
     isAuthenticated: false,
     email: "",
     headerHeight: 0,
-    controlPanelHeight: 0
+    controlPanelHeight: 0,
+    page: "/"
 });
 
-export function isAuthenticated() {
-    return get(appStore).isAuthenticated;
+export function redirect(path) {
+    path="/"
 }
