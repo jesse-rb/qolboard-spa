01907ff162bf8ca64a97285aebdb73aa876107be
jesse-rb <jesse.reynekebarnard@gmail.com>
2025-02-05 21:47:28 +1300

    Fix canvas shared cursor client offset positioning

--- src/lib/components/Canvas/Cursors.svelte
+++ src/lib/components/Canvas/Cursors.svelte
@@ -6,17 +6,42 @@
     export let cursors: Record<string, { x: number; y: number }> = {};
 
     const canvasStore: Writable<Canvas> = getContext("canvasStore");
+
+    const offset = 0;
+
+    $: canvasZoom = $canvasStore.canvasData.zoom;
+
+    $: canvasWidth = $canvasStore.canvasData.width ?? 0;
+    $: canvasHeight = $canvasStore.canvasData.height ?? 0;
+
+    $: canvasWidthScaled = canvasWidth * canvasZoom;
+    $: canvasHeightScaled = canvasHeight * canvasZoom;
+
+    $: canvasPanX = $canvasStore.canvasData.xPan;
+    $: canvasPanY = $canvasStore.canvasData.yPan;
+
+    $: zoomClientOffsetX = (canvasWidth - canvasWidthScaled) / 2;
+    $: zoomClientOffsetY = (canvasHeight - canvasHeightScaled) / 2;
 </script>
 
 {#each Object.entries(cursors) as [email, pos]}
+    <!--some QUICK MATHS-->
     {@const x =
-        pos.x / $canvasStore.canvasData.zoom + $canvasStore.canvasData.xPan}
-    {@const y = pos.y + $canvasStore.canvasData.yPan}
+        pos.x * canvasZoom +
+        zoomClientOffsetX +
+        canvasPanX * canvasZoom +
+        offset}
+    {@const y =
+        pos.y * canvasZoom +
+        zoomClientOffsetY +
+        canvasPanY * canvasZoom +
+        offset}
     <div
         class="pointer-events-none fixed"
         style="left: {x}px; top: calc({y}px + var(--header-height));"
     >
         <span class="material-icons">mouse</span>
         <span class="text-xs">{email}</span>
+        <span>zoomDx: {$canvasStore.canvasData.zoomDx}</span>
     </div>
 {/each}
