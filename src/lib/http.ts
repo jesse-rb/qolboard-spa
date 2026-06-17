527edbffb0d04a1e53cf3cb90751f257ebbae5a7
Jesse Reyneke-Barnard <43373663+jesse-rb@users.noreply.github.com>
2026-06-18 00:47:33 +1200

    Add handling for new auth refresh token flow (#40)

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,4 +1,5 @@
 import { goto } from "$app/navigation";
+import { appStore } from "./store";
 
 export type TypeHttpMethods = "GET" | "POST" | "PUT" | "DELETE";
 
@@ -6,7 +7,7 @@ export function getApiHost(): string {
     return import.meta.env.VITE_API_HOST;
 }
 
-export async function request(method: TypeHttpMethods, path: string, requestBody: object | null = null): Promise<Response> {
+export async function request(method: TypeHttpMethods, path: string, requestBody: object | null = null, allowRefresh: boolean = true): Promise<Response> {
     const apiHost = getApiHost();
     const url = `${apiHost}/${path}`;
     const resp = await fetch(url, {
@@ -19,6 +20,26 @@ export async function request(method: TypeHttpMethods, path: string, requestBody
     });
     if (resp.status == 429) {
         goto("/error?status=429");
+    }
+    if (resp.status == 401) {
+        if (allowRefresh) {
+            const refreshResp = await request("POST", "auth/refresh", null, false)
+            if (refreshResp.status === 200) {
+                // Only if refresh was successful, attempt to replay request (another refresh attempt will not be allowed)
+                const resp = request(method, path, requestBody, false)
+                return resp
+            }
+        }
+
+        // If refresh was unsuccessful, update state to report a logged out state
+        appStore.update((store) => {
+            store.checkedIsAuthenticated = true;
+            store.isAuthenticated = false;
+            store.user.email = "";
+            store.user.id = "";
+
+            return store;
+        });
     }
 
     return resp
